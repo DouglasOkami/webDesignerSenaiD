42f5e4e76466984cd295081402e6f0cdcd6c73d5
DouglasOkami <douglassityo@gmail.com>
2021-11-23 14:39:28 -0300

    atividade 2 do dia 23/11 encerrada

--- aulaDezesete/js/arquivo.js
+++ aulaDezesete/js/arquivo.js
@@ -4,3 +4,13 @@ function calsal(){
     nsal = sal + ((sal*25)/100)
     document.getElementById('idnsal').value = nsal;
 }
+
+function Media(){
+    let nota1 = Number(document.getElementById('idnota1').value);
+    let nota2 = Number(document.getElementById('idnota2').value);
+    let nota3 = Number(document.getElementById('idnota3').value);
+    let nota4 = Number(document.getElementById('idnota4').value);
+    let mediaAluno;
+    mediaAluno = (nota1 + nota2 + nota3 + nota4)/4;
+    document.getElementById('idmedia').value = mediaAluno;
+}
