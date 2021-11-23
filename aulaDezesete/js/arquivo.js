4e4813a8a79fa78a4dd46483443faa5eefa928ef
DouglasOkami <douglassityo@gmail.com>
2021-11-23 16:27:50 -0300

    atividade 3,4 e 5 do dia 23/11 encerrada

--- aulaDezesete/js/arquivo.js
+++ aulaDezesete/js/arquivo.js
@@ -14,3 +14,40 @@ function Media(){
     mediaAluno = (nota1 + nota2 + nota3 + nota4)/4;
     document.getElementById('idmedia').value = mediaAluno;
 }
+
+function ndobro(){
+    let numero = parseInt(document.getElementById('num').value);
+    document.getElementById('dnum').value = numero * 2;
+}
+
+function nsoma(){
+    let nume1 = Number(document.getElementById('num1').value);
+    let nume2 = Number(document.getElementById('num2').value);
+    let soma;
+    soma = nume1 + nume2;
+    document.getElementById('resultado').value = soma;
+}
+
+function nquadrado(){
+    let nume1 = Number(document.getElementById('num1').value);
+    let quadrado;
+    quadrado = nume1 * nume1;
+    document.getElementById('resultado').value = quadrado;
+}
+
+function nsomaT(){
+    let nume1 = Number(document.getElementById('num1').value);
+    let nume2 = Number(document.getElementById('num2').value);
+    let nume3 = Number(document.getElementById('num3').value);
+    let soma;
+    soma = nume1 + nume2 + nume3;
+    document.getElementById('resultado').value = soma;
+}
+
+function narea(){
+    let base = Number(document.getElementById('num1').value);
+    let altura = Number(document.getElementById('num2').value);
+    let areaT;
+    areaT = (base * altura)/2;
+    document.getElementById('resultado').value = areaT;
+}
