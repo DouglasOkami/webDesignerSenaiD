8106062bdf4d72b50f7630bbe2ea9aa6eb16d9ba
DouglasOkami <douglassityo@gmail.com>
2021-11-26 15:58:00 -0300

    aula de introdução de Javascrit

---
+++ aulaVinte/js/Primeira.js
@@ -0,0 +1,17 @@
+/*
+let a = 5;
+let b = 8;
+let c = 0;
+
+c = a;
+a = b;
+b = c;
+
+console.log("Valor de a :"+ a);
+console.log("Valor de b :"+ b);
+*/
+let a = 5;
+let b = "10";
+let c = a + b;
+console.log(c);
+ 
