0cdc6b7077801da65b1e5fee8e77f7e62483b945
DouglasOkami <douglassityo@gmail.com>
2021-11-30 16:34:35 -0300

    organizando funções

--- aulaVinteEUm/js/Funcoes.js
+++ aulaVinteEUm/js/Funcoes.js
@@ -12,3 +12,38 @@ function mudarPVermelho(){
 function mudarPVerde(){
     document.getElementById("paragrafo").style.color = 'green';
 }
+
+function myButton(){
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.fontSize = "40px";
+    document.getElementById("demo").style.background = "yellow";
+    document.getElementById("demo").style.fontFamily = "Arial";
+}
+/*
+function calculadora(){
+    document.getElementById("resultado").value = parseFloat(document.getElementById("numero1").value) + 
+    parseFloat(document.getElementById("numero2").value);
+}
+
+function calculadora(){
+    let n1 = parseInt(document.getElementById("numero1").value);
+    let n2 = parseInt(document.getElementById("numero2").value);
+    let calcular = n1 + n2;
+    document.getElementById("resultado").value = calcular;
+}
+*/
+
+function coletandoDados(){
+    let n1 = parseInt(document.getElementById("numero1").value);
+    let n2 = parseInt(document.getElementById("numero2").value);
+    somar(n1, n2);
+}
+
+function somar(n1, n2){
+    let res = n1 + n2;
+    imprimirDados(res);
+}
+
+function imprimirDados(res){
+    document.getElementById("resultado").value = res;
+}
